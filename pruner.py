171131d0eac852b3d1fb6279ef0618abf4b7dd76
Alexander Koumis <me@alexander.computer>
2015-11-20 00:32:13 -0800

    Updated pruner

--- pruner.py
+++ pruner.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 import json
+import os
 import sys
+import timeit
 from os import makedirs
-from os.path import basename, isdir, isfile, join
+from os.path import basename, dirname, getsize, isdir, isfile, join, realpath
 from shutil import copy2
 from StringIO import StringIO
 from tld import get_tld
 from tld.utils import update_tld_names
 
 import cv2
-
 cv2gpu_imported = True
 try:
     import cv2gpu
@@ -21,10 +22,16 @@ except ImportError:
 
 
 scraped_dir = '/home/dev/Datasets/scrapy_collected/'
-json_path = join(scraped_dir, 'lol4.json')
-cascade_cpu_path = join(scraped_dir, 'haarcascade_frontalface_default.xml')
-cascade_gpu_path = join(scraped_dir, 'haarcascade_frontalface_default_cuda.xml')
+json_path_orig = join(scraped_dir, 'lol5.json')
+processed_dir = join(scraped_dir, 'processed')
+json_path_pruned = join(processed_dir, 'lol5_pruned.json')
 
+cascade_dir = join(dirname(realpath(__file__)), 'haarcascades')
+cascade_cpu_path = join(cascade_dir, 'haarcascade_frontalface_default.xml')
+cascade_gpu_path = join(cascade_dir, 'haarcascade_frontalface_default_cuda.xml')
+
+if isfile(json_path_pruned):
+    os.remove(json_path_pruned)
 
 def load_json(path):
     with open(path, 'r') as f:
@@ -61,26 +68,34 @@ def sort_domains(image_obj):
     return image_obj_ret
 
 def write_json(image_obj):
-    pruned_path = '{}_pruned.json'.format(json_path.split('.')[0])
-    with open(pruned_path, 'w') as outfile:
+    with open(json_path_pruned, 'w') as outfile:
         outfile.write(json.dumps(image_obj, sort_keys=True, indent=4, ensure_ascii=False))
+
+def count_images(image_obj):
+    num_images = 0
+    for tld in image_obj:
+        images = image_obj[tld]
+        for url in images:
+            num_images += 1
+    return num_images
 
 def separate_images(image_obj):
 
+    size_file_max = 200000
+
     def prune_func_gpu(image_path):
-        if isfile(image_path):
-            print image_path
+        if getsize(image_path) < size_file_max:
             faces = cv2gpu.find_faces(image_path)
-            if len(faces) > 0:
-                return True
+            if faces:
+                return len(faces) > 0
         return False
 
     def prune_func_cpu(image_path):
-        if isfile(image_path):
+        if getsize(image_path) < size_file_max:
             image = cv2.imread(image_path, 0)
-            faces = cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
-            if len(faces) > 0:
-                return True
+            faces = cascade.detectMultiScale(image)
+            if faces:
+                return len(faces) > 0
         return False
 
     if not isfile(cascade_gpu_path) or not isfile(cascade_cpu_path):
@@ -92,29 +107,37 @@ def separate_images(image_obj):
     if cv2gpu_imported:
         prune_func = prune_func_gpu
         if cv2gpu.is_cuda_compatible():
+            print 'Using GPU detector'
             cv2gpu.init_gpu_detector(cascade_gpu_path)
         else:
+            print 'Using CPU detector'
             cv2gpu.init_cpu_detector(cascade_cpu_path)
     else:
         prune_func = prune_func_cpu
         cascade = cv2.CascadeClassifier(cascade_cpu_path)
 
+    image_num_max = count_images(image_obj)
+    image_num_curr = 0
+
     for tld in image_obj:
-        out_dir = join(scraped_dir, 'processed', tld)
+        out_dir = join(processed_dir, tld)
+        if not isdir(out_dir):
+            makedirs(out_dir)
         images = image_obj[tld]
         for url in images:
             path_orig = images[url]
-            base = basename(path_orig)
-            path_new = join(out_dir, base)
-            if prune_func(path_orig):
-                if not isdir(out_dir):
-                    makedirs(out_dir)
+            path_new = join(out_dir, basename(path_orig))
+            print 'Image {} / {}, ({})'.format(image_num_curr, image_num_max, getsize(path_orig))
+            image_num_curr += 1
+            if not prune_func(path_orig):
+                continue
+            if not isfile(path_new):
                 copy2(path_orig, path_new)
-                images[url] = path_new
+            images[url] = path_new
     return image_obj
 
 def main():
-    images_list = load_json(json_path)
+    images_list = load_json(json_path_orig)
     images_list = prune_paths(images_list)    
     image_obj = prune_nonexisting(images_list)
     image_obj = sort_domains(image_obj)
@@ -122,4 +145,7 @@ def main():
     write_json(image_obj)
 
 if __name__ == '__main__':
+    start_time = timeit.default_timer()
     main()
+    elapsed = timeit.default_timer() - start_time
+    print 'Process took this long: {}'.format(elapsed)
